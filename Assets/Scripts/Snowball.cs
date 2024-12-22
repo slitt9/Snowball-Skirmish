453815a4c207eef57ba8118eca5e8562f0562bdc
slitt9 <sahillitt@hotmail.com>
2024-12-21 20:40:32 -0800

    added Unity files

---
+++ Assets/Scripts/Snowball.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snowball : MonoBehaviour
+{
+    public float ballSpeed;
+    public GameObject snowBallEffect;
+    private Rigidbody2D theRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(ballSpeed * transform.localScale.x, 0);
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player1")
+        {
+            FindObjectOfType<GameManager>().HurtP1();
+        }
+        if (other.tag == "Player2")
+        {
+            FindObjectOfType<GameManager>().HurtP2();
+        }
+        Instantiate(snowBallEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
